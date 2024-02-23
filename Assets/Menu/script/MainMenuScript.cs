db0b9adb6fba75c1180fba948343bde1a55d2f3e
davidnina2 <57233286+davidnina2@users.noreply.github.com>
2024-02-23 14:02:00 -0500

    Ajout des menu
    Ajout des menus suivant. Principal, Pause et de fin. pour ouvrir le menu pause il faut cliquer sur b

---
+++ Assets/Menu/script/MainMenuScript.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Unity.VRTemplate
+{
+    public class MainMenuScript : MonoBehaviour
+    {
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        public void PlayGame()
+        {
+            Application.LoadLevel("SampleScene");
+        }
+    }
+}

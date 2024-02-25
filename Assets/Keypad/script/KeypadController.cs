62ad7043da72578fc6d6a85ca06e971a14777b76
jordan1197 <jordanb97@hotmail.com>
2024-02-24 21:05:52 -0500

    logic keypad fonctionnel

--- Assets/Keypad/script/KeypadController.cs
+++ Assets/Keypad/script/KeypadController.cs
@@ -7,27 +7,23 @@ using System;
 
 public class KeypadController : MonoBehaviour
 {
-    public List<int> password = new List<int>();
+    public List<int> correctPassword = new List<int>();
     public int passwordLength = 4;
 
     private List<int> inputPasswordList = new List<int>();
 
     [SerializeField] private TMP_Text codeDisplay;
-    [SerializeField] private float resetTime = 2f;
+    [SerializeField] private float resetTime = 1f;
     [SerializeField] private string SuccessText;
     [Space(5f)]
     [Header("Keypad Entry Events")]
     public UnityEvent onCorrectPassword;
     public UnityEvent onIncorrectPassword;
 
-    public bool allowMultipleActivation = true;
-    private bool hasUsedCorrectCode = false;
-    public bool HasUsedCorrectCode { get { return  hasUsedCorrectCode; }}
+    private bool hasBeenUsed = false;
 
     public void UserNumberEntry(int selectedNumber)
     {
-        Debug.Log("usernumber entry");
-        Debug.Log(selectedNumber);
         if (inputPasswordList.Count >= passwordLength)
             return;
 
@@ -38,12 +34,70 @@ public class KeypadController : MonoBehaviour
 
     private void UpdateDisplay()
     {
-        Debug.Log("update dysplay");
         codeDisplay.text = string.Empty;
         
         foreach (int i in inputPasswordList)
         {
             codeDisplay.text += i.ToString();
         }
+    }
+
+    public void DeleteEntry()
+    {
+        if(inputPasswordList.Count > 0 && !hasBeenUsed)
+        {
+            inputPasswordList.RemoveAt(inputPasswordList.Count - 1);
+            UpdateDisplay();
+        }            
+    }
+
+    public void CheckPassword()
+    {
+        if(correctPassword.Count == inputPasswordList.Count)
+        {
+            for (int i = 0; i < correctPassword.Count; i++)
+            {
+                if (inputPasswordList[i] != correctPassword[i])
+                {
+                    IncorrectPassword();
+                    return;
+                }
+            }
+
+            CorrectPassword();
+        }
+        else
+            IncorrectPassword();
+
+    }
+
+    private void CorrectPassword()
+    {
+        onCorrectPassword.Invoke();
+        codeDisplay.text = SuccessText;
+        SetDisplayColor(Color.green);
+        hasBeenUsed = true;
+    }
+
+    private void IncorrectPassword()
+    {
+        SetDisplayColor(Color.red);
+        onIncorrectPassword.Invoke();        
+
+        StartCoroutine(ResetKeyCode());
+    }
+
+    IEnumerator ResetKeyCode()
+    {
+        yield return new WaitForSeconds(resetTime);
+
+        inputPasswordList.Clear();
+        SetDisplayColor(Color.white);
+        codeDisplay.text = "Enter code ...";
+    }
+
+    private void SetDisplayColor(Color color)
+    {
+        codeDisplay.color = color;
     }
 }
